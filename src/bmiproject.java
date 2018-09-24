81d82ac1ca24cc68b481f06f74b70aa6f0fb99fe
Abdi Sirat <abdikadar.sirat.2021@student.lhschools.org>
2018-09-24 13:57:25 -0400

    testing BMI calculations

--- src/bmiproject.java
+++ src/bmiproject.java
@@ -1,64 +1,120 @@
+import java.text.MessageFormat;
 import java.util.Scanner;
 
 public class bmiproject {
+    //(weight (in lbs)*703)/ (height (in inches)^2)
 
-    public static void main(String[]  args){
+    public static double calculate(int weight,int height){
+        weight= (weight*703);
+        height= height*weight;
+        return weight/height;
+    }
+    public static String message(double BMI) {
+        if (BMI < 730) {
+            return "you are to fat";
+        } else if (BMI < 15) {
+            return "you need to eat more!";
+        } else {
+            return "you BMI is perfect";
+        }
+    }
+
+    public static void main (String [] args) {
         //declare
-        Scanner keyboard;
-         String name;
-         int height;
-        int feet;
-        int inch;
-        double meter;
-        int BMI;
-        int kg;
-        int lbs;
-
-
-
-        //initialize
-        keyboard= new Scanner(System.in);
-
-
-        System.out.println("Hello my name is Abdi, what is yours?");
-        name=keyboard.nextLine();
-        System.out.println("what is your height in feet?");
-        feet= keyboard.nextInt();
+        Scanner keyboard = new Scanner(System.in);
         System.out.println("what is your height in inches?");
-        inch=keyboard.nextInt();
+        int height= keyboard.nextInt();
+        System.out.println("what is your weight in pounds?");
+        int weight= keyboard.nextInt();
+        double BMI;
+        BMI = calculate(weight,height);
+
+
+//        //initialize
+//        keyboard= new Scanner(System.in);
+//
+//
+//        System.out.println("Hello my name is Abdi, what is yours?");
+//        name=keyboard.nextLine();
+//        System.out.println("what is your height in feet?");
+//        feet= keyboard.nextInt();
+//        System.out.println("what is your height in inches?");
+//        inch=keyboard.nextInt();
+//        System.out.println("what is your weight in pounds");
+//        weight= keyboard.nextInt();
+//
+//
+//        inch= inch + (feet*12);
+//        meter =  (inch * 0.0254);
+//        kg= keyboard.nextInt();
 
 
 
-        inch= inch + (feet*12);
-        meter =  (inch * 0.0254);
 
-
-
-
-
-
-
-
-
-
-        BMI= kg/(meter*meter);
-
-
-
-
-
-
-
-
-
-
+            }
 
         }
 
 
 
 
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
