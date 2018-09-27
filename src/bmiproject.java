0e06853ed0ae905516ab6f5a55317d577cde4889
Abdi Sirat <abdikadar.sirat.2021@student.lhschools.org>
2018-09-27 12:59:05 -0400

    finished BMI project

--- src/bmiproject.java
+++ src/bmiproject.java
@@ -1,40 +1,47 @@
 import java.util.Scanner;
 
 public class bmiproject {
-    //(weight (in lbs)*703)/ (height (in inches)^2)
+    public static void main(String[] args) {
 
-    public static double calculate(int weight,int height){
-        weight= (weight*703);
-        height= height*weight;
-        return weight/height;
-    }
-    public static String message(double BMI) {
-        if (BMI < 730) {
-            return " you are to fat";
-        } if (BMI < 15) {
-            return " you need to eat more!";
-        } else return " your BMI is perfect";
-    }
+        final double KILOGRAMS_PER_POUND = 0.453;
+        final double METERS_PER_INCH = 0.026;
 
-    public static void main (String [] args) {
-        //declare
-        Scanner keyboard = new Scanner(System.in);
-        System.out.println("what is your height in inches?");
-        int height = keyboard.nextInt();
-        System.out.println("what is your weight in pounds?");
-        int weight = keyboard.nextInt();
-        double BMI;
-        BMI = calculate(weight, height);
-        String output = BMI + "" + message(BMI);
-        System.out.println(output);
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter weight in pounds: ");
+        double weight = input.nextDouble();
+
+        System.out.print("Enter height in inches: ");
+        double height = input.nextDouble();
+
+        double weightInKilogram = weight * KILOGRAMS_PER_POUND;
+        double heightInMeters = height * METERS_PER_INCH;
+        double bmi = weightInKilogram /
+                (heightInMeters * heightInMeters);
+
+        System.out.printf("Your Body Mass Index is " + bmi);
+
+        if (bmi < 16)
+            System.out.println("You are seriously underweight");
+        else if (bmi < 18)
+            System.out.println("You are underweight");
+        else if (bmi < 24)
+            System.out.println("You are normal weight");
+        else if (bmi < 29)
+            System.out.println("You are overweight");
+        else if (bmi < 35)
+            System.out.println("You are seriously overweight");
+        else
+            System.out.println("You are gravely overweight");
     }
+}
 
 
 //
 
 
 
-            }
+
 
 
 
